c77a923c1b9b8c17b6b8d1a44043eab4f0e717e4
valerie-byte <vn1.thouvenot@gmail.com>
2025-01-25 13:30:15 +0100

    création des composants pageaccueil, header, et PresentationComponent

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import logo from'../images/logo.png'
+import iconfacebook from '../images/header/logo-facebook.svg'
+import iconInstagram from '../images/header/logo-instagram.svg'
+import '../styles/header.css'
+
+export default function Header () {
+    return(
+        <header className='headerContainer'>
+            <img src={logo} alt='mon logo'/>
+            <h1 className='allerta-regular'>Accueil</h1>
+            <h1 className='allerta-regular'>Galerie</h1>
+            <h1 className='allerta-regular'>Tarif et prestations</h1>
+            <h1 className='allerta-regular'>Contact</h1>
+            <img src={iconfacebook} alt='logo-facebook'></img>
+            <img src={iconInstagram} alt='logo-instagram'></img>
+        </header>
+    )
+}
---
+++ src/components/pageAccueil.js
@@ -0,0 +1,14 @@
+import Header from "./header";
+import PresentationComponent from "./PresentationComponent";
+
+function PageAccueil () {
+    return (
+        <div>     
+        <Header></Header>
+        <PresentationComponent></PresentationComponent>
+        </div>
+    )
+};
+
+export default PageAccueil;
+
